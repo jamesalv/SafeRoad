14327671d6a9fa6faf9346d59c4741c9ae6cb5fb
jamesalv <jamesalvdhan@gmail.com>
2024-12-04 23:17:34 +0700

    Implement functions to generate random points and capture street view images within a specified radius

--- server/street_view.py
+++ server/street_view.py
@@ -1,5 +1,7 @@
 from io import BytesIO
+import math
 import os
+import random
 from dotenv import load_dotenv
 import requests
 from PIL import Image
@@ -48,6 +50,58 @@ def get_street_view_image(lat, lng, heading):
     image = Image.open(BytesIO(response.content))
     return image
 
+def get_pano_id(lat, lng):
+    """
+    Get panorama id given the latitude and longitude
+    * helps to make sure that the generated points are valid coordinates
+    """
+    metadata_url = f"{STREET_VIEW_URL}/metadata?location={lat},{lng}&key={API_KEY}"
+    response = requests.get(metadata_url)
+    data = response.json()
+    return data.get("pano_id")
+
+def generate_points_in_radius(center_lat, center_lon, radius_km, num_points):
+    """
+    Generate random points within a given radius from a center point
+    """
+    
+    points = []
+    i = 0
+    while i < num_points:
+        angle = math.radians(random.uniform(0, 360))
+        distance = random.uniform(0, radius_km)
+
+        lat_offset = distance / 111.32
+        lon_offset = distance / (111.32 * math.cos(math.radians(center_lat)))
+
+        new_lat = center_lat + (lat_offset * math.cos(angle))
+        new_lon = center_lon + (lon_offset * math.sin(angle))
+        
+        pano_id = get_pano_id(new_lat, new_lon)
+        if pano_id:
+            points.append((new_lat, new_lon))
+            i += 1
+
+    return points
+
+
+def capture_images_in_radius(center_lat, center_lon, radius_km, num_images):
+    """
+    Capture street view images within a given radius from a center point
+    """
+
+    points = generate_points_in_radius(center_lat, center_lon, radius_km, num_images)
+    images = []
+
+    for point in points:
+        lat, lon = point
+    
+        # Get images in 4 directions
+        for heading in range(0, 360, 90):
+            image = get_street_view_image(lat, lon, heading)
+            images.append(image)
+
+    return images
 
 # Test doang sich
 # if __name__ == "__main__":
@@ -55,5 +109,6 @@ def get_street_view_image(lat, lng, heading):
 #     lng = 106.8271528
 #     street_name = get_street_name(lat, lng)
 #     print(street_name)
-#     img = get_street_view_image(lat, lng, 0)
-#     img.show()
+#     images = capture_images_in_radius(lat, lng, 0.5, 2)
+#     for image in images:
+#         image.show()
